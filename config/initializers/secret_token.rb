84b56c6a6e74dfd9a2d9ad8b85521c05086b6224
Joe Ellis <joe@squarefour.net>
2010-11-12 10:28:00 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theanswertoeverything::Application.config.secret_token = '982d3d7fa27fd97f10627d782cf842359c2f0df4f52433c0560feecccb8bbdbae704c001064ad283ab249e7cb45a7ccc9b39921d75ce14efe6bedaf02f4d8929'
